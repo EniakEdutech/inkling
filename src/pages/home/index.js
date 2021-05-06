db87b915060eae10438fb32b440c6d52bcc43d8d
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-06 20:21:09 +0530

    added about us link

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,6 +6,7 @@ export const Home = () => {
     return <div className="flex column" style={{ rowGap: "10px" }}>
         <Header />
         <p>Home page</p>
+        <Link to="/aboutus">About Us</Link>
         <Button className="w-100 w-md-50 w-lg-25" primary>Test button</Button>
         <Button className="w-100 w-md-50 w-lg-25" secondary>Test button</Button>
         <Button className="w-100 w-md-50 w-lg-25" helper>Test button</Button>
