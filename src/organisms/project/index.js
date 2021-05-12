b694d59a2673efa9ceea5e83ec49e9db43b41f14
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-12 10:38:26 +0530

    portfolio page done

--- src/organisms/project/index.js
+++ src/organisms/project/index.js
@@ -7,22 +7,40 @@ import { Image } from "../../atoms/image"
 
 const StyledDiv = styled.div`
 padding: 10px;
-
 >.image {
-    display: none;
+    width: 100%;
 }
 
-@media screen and (min-width: 992px) {}
+@media screen and (hover:hover) {
+    >.title {
+        display: none;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+    }
+    :hover > .title {
+        display: flex;
+        background-color: rgba(255, 255, 255, 0.5);
+        backdrop-filter: blur(5px);
+    }
+}
 
 @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) and (orientation: portrait) {
     >.image {
-        display: none;
     }
 }
 `
 
 const ProjectImage = styled(Image)`
 height: 25vh;
+@media screen and (min-width: 992px) {
+    height: 40vh;
+}
 
 `
 
@@ -30,12 +48,10 @@ height: 25vh;
 
 export const Project = props => {
     return <StyledDiv {...props} className="relative w-100 w-xmd-50 w-xlg-33">
-        {/* <Heading2>Eniak</Heading2>
-        <Heading4>Description</Heading4> */}
-        <div>
-            <CardTitle>Enaik</CardTitle>
-            <CardSub>Description</CardSub>
+        <div className="title">
+            <CardTitle>{props.title}</CardTitle>
+            <CardSub>{props.description}</CardSub>
         </div>
-        <ProjectImage />
+        <ProjectImage src={props.image} className="image" alt={props.alt} />
     </StyledDiv>
 }
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,16 +1,127 @@
-import { Fragment } from 'react'
+import { Fragment, Component } from 'react'
+import styled from 'styled-components'
 // components
 import { NavMenu } from '../../molecules/navmenu'
 import { Project } from '../../organisms/project'
 
-export const Portfolio = () => {
-    return <Fragment>
-        <NavMenu />
-        <div className="flex wrap w-100 w-md-80 w-xlg-50 margin-center">
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-        </div>
-    </Fragment>
+const CategoryContainerDiv = styled.div`
+margin: 20px 0;
+overflow-x: auto;
+white-space: nowrap;
+
+@media screen and (min-width: 768px) {
+    justify-content: center;
+}
+`
+
+const CategoryDiv = styled.div`
+padding: 10px 30px;
+border: 1px solid var(--primary-light-variant);
+color: var(--primary-light-variant);
+
+&.selected {
+    border: 1px solid var(--primary-dark-variant);
+    background-color: var(--primary-dark-variant);
+    color: var(--primary-text);
+}
+`
+
+export class Portfolio extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            projects: [
+                {
+                    title: "Eniak",
+                    description: "Eniak description",
+                    image: "https://ik.imagekit.io/bizwem110d/default-image.jpg",
+                    alt: "Eniak",
+                    // link="/eniak",
+                    category: ['Design', 'Development']
+                },
+                {
+                    title: "ITK",
+                    description: "Information Technology Kindergarden",
+                    image: "https://ik.imagekit.io/bizwem110d/default-image.jpg",
+                    alt: "ITK",
+                    category: ['Design', 'Curriculum Design']
+                },
+                {
+                    title: "90 min",
+                    description: "90 minute application challenge",
+                    image: "https://ik.imagekit.io/bizwem110d/default-image.jpg",
+                    alt: "90min",
+                    category: ['Development']
+                },
+                {
+                    title: "Code for all",
+                    description: "Code for all",
+                    image: "https://ik.imagekit.io/bizwem110d/default-image.jpg",
+                    alt: "Code for all",
+                    category: ['Curriculum Design']
+                }
+            ],
+            showProjects: [],
+            category: 'all'
+        }
+    }
+
+    componentDidMount() {
+        this.categorySelector('all')
+    }
+
+    categorySelector = category => {
+        if (category === 'all') {
+            this.setState({
+                showProjects: this.state.projects,
+                category: 'all'
+            })
+        } else {
+            let showProjectList = []
+            this.state.projects.map(project => {
+                if (project.category.includes(category)) {
+                    showProjectList.push(project)
+                }
+                return null
+            })
+            this.setState({
+                showProjects: showProjectList,
+                category: category,
+            })
+        }
+    }
+
+    render() {
+        return <Fragment>
+            <NavMenu />
+            <div className="flex column w-100 w-md-80 w-xlg-50 margin-center">
+                <CategoryContainerDiv className="flex">
+                    <CategoryDiv
+                        onClick={() => this.categorySelector('all')}
+                        className={this.state.category === 'all' && "selected"}>
+                        All Projects
+                        </CategoryDiv>
+                    <CategoryDiv
+                        onClick={() => this.categorySelector('Design')}
+                        className={this.state.category === 'Design' && "selected"}>
+                        Design
+                        </CategoryDiv>
+                    <CategoryDiv
+                        onClick={() => this.categorySelector('Development')}
+                        className={this.state.category === 'Development' && "selected"}>
+                        Development
+                        </CategoryDiv>
+                    <CategoryDiv
+                        onClick={() => this.categorySelector('Curriculum Design')}
+                        className={this.state.category === 'Curriculum Design' && "selected"}>
+                        Curriculum Design
+                    </CategoryDiv>
+                </CategoryContainerDiv>
+                <div className="flex wrap">
+                    {this.state.showProjects.map(project => <Project key={project.title} title={project.title} description={project.description} image={project.image} alt={project.alt} />)}
+                </div>
+            </div>
+        </Fragment>
+    }
 }
