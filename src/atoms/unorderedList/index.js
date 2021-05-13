f283057c6267589c9ffcbb940e6c65855fc496db
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-13 10:46:00 +0530

    Added padding start

--- src/atoms/unorderedList/index.js
+++ src/atoms/unorderedList/index.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-const StyledUl = styled.ul``
+const StyledUl = styled.ul`
+padding-inline-start: 20px;
+`
 
 export const UnorderedList = props => {
     return <StyledUl {...props}>{props.children}</StyledUl>
