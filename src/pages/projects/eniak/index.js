f13f4b32d4b24a055c6757da9319bcbd9ddeb3b0
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-13 16:14:42 +0530

    all pages done

--- src/pages/projects/eniak/index.js
+++ src/pages/projects/eniak/index.js
@@ -1,27 +1,21 @@
 import { Fragment } from 'react'
-import styled from 'styled-components'
+// special components
+import ScrollToTopOnMount from '../../../atoms/scrollToTop'
 // components
 import { NavMenu } from '../../../molecules/navmenu'
+import { NavButtons, StickyNavButtons } from '../../../molecules/navbuttons'
 import { Title } from '../../../atoms/title'
 import { Heading1 } from '../../../atoms/heading1'
 import { Heading4 } from '../../../atoms/heading4'
 import { ImportantText } from '../../../atoms/importantText'
 import { Paragraph } from '../../../atoms/paragraph'
-import { Strong } from '../../../atoms/strong'
-import { Image } from '../../../atoms/image'
-
-const StyledNavSection = styled.div`
-margin: 0 5px;
-
-@media screen and (min-width: 768px) {
-    margin: unset;
-}
-`
+import { Banner } from '../../../atoms/banner'
 
 export const Eniak = props => {
     return <Fragment>
+        <ScrollToTopOnMount />
         <NavMenu />
-        <div class="flex column r-g-1 w-xmd-80 w-xlg-50 margin-center my-5">
+        <div class="flex column r-g-1 w-80 w-xlg-50 margin-center my-2 my-xmd-4 my-lg-5">
             <div className="flex column lg-row">
                 <Title className="flex-lg-3">Eniak</Title>
                 <Heading1 className="flex-lg-7">Making educational and technological reform.</Heading1>
@@ -33,8 +27,8 @@ export const Eniak = props => {
                     </ImportantText>
                 </div>
                 <div className="flex-lg-7 flex column r-g-1">
-                    <Heading4 className="flex-lg-7">Context</Heading4>
-                    <Paragraph className="flex-lg-7">
+                    <Heading4>Context</Heading4>
+                    <Paragraph>
                         Eniak (Educational network of information and knowledge) works in the EduTech sector creating
                         innovative and unique products/services for greater educational and technological reform. Eniak
                         is a parent company to its pilot product ITK that explores education and technology. Not only is
@@ -43,22 +37,12 @@ export const Eniak = props => {
                 </Paragraph>
                 </div>
             </div>
-            <StyledNavSection className="flex w-md-100 space-between margin-center">
-                <div className="flex align-center c-g-1">
-                    <span>&larr;</span>
-                    <Strong>previous project</Strong>
-                </div>
-                <div className="flex align-center c-g-1">
-                    <Strong>next project</Strong>
-                    <span>&rarr;</span>
-                </div>
-            </StyledNavSection>
+            <NavButtons prev="/portfolio/mainbhiambani" next="/portfolio/tcscfeedback" />
         </div>
-        <div className="w-xlg-66 margin-center">
-            <Image src="https://ik.imagekit.io/bizwem110d/inkling/Eniak_project_images/tiny2-01_tOFi8YDp3.png" />
-        </div>
-        <div class="flex column lg-row w-xmd-80 w-xlg-50 margin-center r-g-1 my-4">
-            <Heading1 className="flex-lg-3">A revolutionary way to approach education.</Heading1>
+        <Banner src="https://ik.imagekit.io/bizwem110d/inkling/Eniak_project_images/tiny2-01_tOFi8YDp3.png" />
+        <StickyNavButtons prev="/portfolio/mainbhiambani" next="/portfolio/tcscfeedback" />
+        <div class="flex column lg-row w-80 w-xlg-50 margin-center g-1 my-2 my-md-3 my-lg-4">
+            <Heading1 className="flex-lg-3">A revolutionary way&nbsp;to approach education.</Heading1>
             <div className="flex column flex-lg-7 r-g-1">
                 <Heading4>Design Brief</Heading4>
                 <Paragraph>Eniak as a parent company needs to encompass all its different components (products and
@@ -73,9 +57,7 @@ export const Eniak = props => {
                 </Paragraph>
             </div>
         </div>
-        <div className="w-xlg-66 margin-center">
-            <Image src="https://ik.imagekit.io/bizwem110d/inkling/Eniak_project_images/small-01_ZFX3FoFYE.png" />
-            <Image src="https://ik.imagekit.io/bizwem110d/inkling/Eniak_project_images/ENIAK_ClWusekxn.png" />
-        </div>
+        <Banner src="https://ik.imagekit.io/bizwem110d/inkling/Eniak_project_images/small-01_ZFX3FoFYE.png" />
+        <Banner src="https://ik.imagekit.io/bizwem110d/inkling/Eniak_project_images/ENIAK_ClWusekxn.png" />
     </Fragment>
 }
--- src/pages/projects/mainbhiambani/index.js
+++ src/pages/projects/mainbhiambani/index.js
@@ -1,9 +1,74 @@
-import {NavMenu} from '../../../molecules/navmenu'
-import { Title} from '../../../atoms/title'
+import { Fragment } from 'react'
+// special components
+import ScrollToTopOnMount from '../../../atoms/scrollToTop'
+// components
+import { NavMenu } from '../../../molecules/navmenu'
+import { NavButtons, StickyNavButtons } from '../../../molecules/navbuttons'
+import { Title } from '../../../atoms/title'
+import { Heading4 } from '../../../atoms/heading4'
+import { ImportantText } from '../../../atoms/importantText'
+import { Text } from '../../../atoms/text'
+import { Paragraph } from '../../../atoms/paragraph'
+import { UnorderedList } from '../../../atoms/unorderedList'
+import { ListItem } from '../../../atoms/listItem'
+import { Banner } from '../../../atoms/banner'
 
 export const MainBhiAmbani = props => {
-    return <div>
+    return <Fragment>
+        <ScrollToTopOnMount />
         <NavMenu />
-        <Title>Main Bhi Ambani</Title>
-    </div>
+        <div class="flex column r-g-1 w-80 w-xlg-50 margin-center my-2 my-lg-5">
+            <Title>Main Bhi Ambani</Title>
+            <div className="flex column lg-row r-g-1">
+                <div className="flex-lg-3 flex column r-g-1">
+                    <Heading4>Services</Heading4>
+                    <ImportantText>Web Development, Data Collection strategy, Cloud Architecture.
+                    </ImportantText>
+                </div>
+                <div className="flex-lg-7 flex column r-g-1">
+                    <Heading4>Context</Heading4>
+                    <Paragraph>
+                        We consulted a major social influencer on a social experiment called “Mai Bhi Ambani”. It is a web
+                        app that allows one to metaphorically place oneself into the ownership of the staggering net worth
+                        of Mr. Mukesh Ambani (40,000.000,000,000 INR) and browse through a list of items they wish to buy
+                        using that wealth.
+                </Paragraph>
+                </div>
+            </div>
+            <NavButtons prev="/portfolio/eniak" next="/portfolio/tcscfeedback" />
+        </div>
+        <Banner src="https://ik.imagekit.io/bizwem110d/inkling/Main_bhi_Ambani/mainbhiambani-banner-1_iLApmpYn2.png" />
+        <StickyNavButtons prev="/portfolio/eniak" next="/portfolio/tcscfeedback" />
+        <div className="flex column lg-row g-1 w-80 w-xlg-50 margin-center my-2 my-lg-5">
+            <div className="flex column r-g-1 flex-lg-1">
+                <Heading4>Goal</Heading4>
+                <Paragraph>
+                    The goal of the experiment was to understand what would
+                    people hypothetically buy when they have that kind of
+                    staggering wealth.
+                </Paragraph>
+                <Heading4>Research</Heading4>
+                <Text>Experiment Stages:</Text>
+                <UnorderedList>
+                    <ListItem>Defining the data structure</ListItem>
+                    <ListItem>Data collection strategy - Application Layer</ListItem>
+                    <ListItem>Analyzing</ListItem>
+                    <ListItem>Distilling the data.</ListItem>
+                </UnorderedList>
+            </div>
+            <div className="flex column r-g-1 flex-lg-1">
+                <Heading4>Challenges</Heading4>
+                <UnorderedList>
+                    <ListItem>Multi-Lingual (As the target market’s primary language as Hindi)</ListItem>
+                    <ListItem>Robust Server Architecture because of the existing social capital of the Influencer</ListItem>
+                </UnorderedList>
+                <Heading4>Learnings</Heading4>
+                <Paragraph>
+                    The question comes first. The questions define the structure
+                    of the data, which enables the insights to be generated.
+                </Paragraph>
+            </div>
+        </div>
+        <Banner src="https://ik.imagekit.io/bizwem110d/inkling/Main_bhi_Ambani/Main_bhi_Ambani_aVzqkXzCy.png" />
+    </Fragment>
 }
