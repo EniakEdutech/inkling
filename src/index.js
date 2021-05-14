fda7ffeb761bfe8b9f6b02fbeac8ead2e29f4888
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-14 20:11:33 +0530

    shared navmenu component added to main index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 // Modules
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
+// Component
+import { NavMenu } from './molecules/navmenu'
 // Pages
 import { Home } from './pages/home'
 import { AboutUs } from './pages/aboutus';
@@ -12,6 +14,7 @@ import { ProjectEniak } from './pages/project-eniak'
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL} >
+      <NavMenu />
       <Switch>
         <Route path="/aboutus" ><AboutUs /></Route>
         <Route path="/portfolio" exact><Portfolio /></Route>
