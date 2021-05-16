d201d24d552d75ab38daa33fdb027262aa24e77a
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-17 04:52:38 +0530

    grayscale only on hover

--- src/molecules/client/index.js
+++ src/molecules/client/index.js
@@ -1,26 +1,17 @@
 import styled from "styled-components"
 import { Image } from "../../atoms/image"
 
-const StyledDiv = styled.div`
-@media screen and (min-width: 1200px) {
-    padding: 1em;
-}
-
-@media screen and (min-width: 2560px) {
-    padding: 3em;
-}
-`
-
 const StyledImage = styled(Image)`
-filter: grayscale(1);
-
-&:hover {
-    filter: grayscale(0.1);
+@media screen and (hover:hover) {
+    filter: grayscale(1);
+    &:hover {
+        filter: grayscale(0);
+    }
 }
 `
 
 export const Client = props => {
-    return <StyledDiv className="w-25 flex align-center center">
+    return <div className="w-50 w-md-33 w-lg-25 py-1 px-1 flex align-center center">
         <StyledImage src={props.src} />
-    </StyledDiv>
+    </div>
 }
