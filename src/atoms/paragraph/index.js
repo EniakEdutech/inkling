8cc3427f7c62674ac7abeb271ead81592f95720d
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-20 21:31:01 +0530

    default font family for paragraphs

--- src/atoms/paragraph/index.js
+++ src/atoms/paragraph/index.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-const StyledPara = styled.p``
+const StyledPara = styled.p`
+font-family: 'Work Sans', sans-serif;
+`
 
 export const Paragraph = props => {
     return <StyledPara {...props}>{props.children}</StyledPara>
