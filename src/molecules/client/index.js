7211c1984bd8e9ed11cfe6ef67650ee323ea8ff6
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-22 22:06:41 +0530

    image sizes as design

--- src/molecules/client/index.js
+++ src/molecules/client/index.js
@@ -3,41 +3,33 @@ import { Image } from "../../atoms/image"
 
 const StyledImage = styled(Image)`
 max-width: 25vw;
-max-height: 10vh;
+max-height: 5vh;
 object-fit: contain;
-${props => props.itk && css`
-padding: 20px 0;
+${props => props.small && css`
+max-height: 3vh;
 `}
 
-@media screen and (min-width: 768px) {
-    ${props => props.itk && css`
-    padding: 30px 0;
-    `}
-}
-
 @media screen and (min-width: 992px) {
-    max-height: 12vh;
-    ${props => props.itk && css`
-    padding: 20px 0;
-    `}
-    ${props => props.snapdeal && css`
-    padding: 25px 0;
+    max-height: 10vh;
+    ${props => props.small && css`
+    max-height: 5vh;
     `}
 }
 
 @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) and (orientation: portrait) {
-    max-height: 8vh;
-    ${props => props.itk && css`
-    padding: 30px 0;
+    max-height: 5vh;
+    ${props => props.small && css`
+    max-height: 3vh;
     `}
 }
 @media screen and (min-width: 2560px) {
-    ${props => props.itk && css`
-    padding: 45px 0;
+    max-height: 5vh;
+    ${props => props.small && css`
+    max-height: 3vh;
     `}
 }
 `
 
 export const Client = props => {
-    return <StyledImage className="py-1" src={props.src} {...props} />
+    return <StyledImage src={props.src} {...props} />
 }
