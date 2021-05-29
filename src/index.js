892187c081ba3a453490e49234ced2f20ee59f2a
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-05-29 09:33:51 +0530

    added itk page

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import { Portfolio } from './pages/portfolio'
 import { Eniak } from './pages/projects/eniak'
 import { MainBhiAmbani } from './pages/projects/mainbhiambani'
 import { TCSCFeedback } from './pages/projects/tscsfeedback'
+import { Itk } from './pages/projects/itk'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,6 +25,7 @@ ReactDOM.render(
         <Route path="/portfolio/eniak"><Eniak /></Route>
         <Route path="/portfolio/mainbhiambani"><MainBhiAmbani /></Route>
         <Route path="/portfolio/tcscfeedback"><TCSCFeedback /></Route>
+        <Route path="/portfolio/itk"><Itk /></Route>
         <Route path="/contactus"><ContactUs /></Route>
         <Route path="/" ><Home /></Route>
       </Switch>
