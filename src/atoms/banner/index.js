8a49702c869a389d72e1e24a383918501bc9fb3c
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-06-05 06:30:10 +0530

    added contain property to banner

--- src/atoms/banner/index.js
+++ src/atoms/banner/index.js
@@ -1,14 +1,14 @@
 import { Image } from "../image"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 const StyledImage = styled(Image)`
-object-fit: cover;
+object-fit: ${props => props.contain ? 'contain' : 'cover'};
 max-width: 100vw;
 max-height: 100vh;
 `
 
 export const Banner = props => {
     return <div className={`d-flex center col-xlg-8 margin-center ${props.className}`}>
-        <StyledImage src={props.src} />
+        <StyledImage src={props.src} {...props} />
     </div>
 }
